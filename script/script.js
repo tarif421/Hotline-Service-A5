87dcc94495e6f197738a03e788ea4a5441fabd08
tarif421 <tarif305@gmail.com>
2025-08-28 21:57:39 +0600

    history added

--- script/script.js
+++ script/script.js
@@ -5,13 +5,32 @@ function getElement(id) {
 
 const callBtns = document.getElementsByClassName("call-btn");
 const copyBtns = document.getElementsByClassName("copy-btn");
+const startNum = document.getElement("star-num");
 
 for (let callBtn of callBtns) {
   callBtn.addEventListener("click", function () {
     const card = callBtn.parentNode.parentNode;
     const hotlineText = card.querySelector("p").innerText;
     const hotlineNum = card.querySelector("h1").innerText;
-    console.log(hotlineText);
-    alert(hotlineText + " " + hotlineNum);
+
+    alert("Calling " + hotlineText + " " + hotlineNum);
+
+    // add history
+    const historyContainer = getElement("history-container");
+    const now = new Date();
+    const time = now.toLocaleTimeString();
+
+    const div = document.createElement("div");
+    div.className = "flex justify-between items-center p-5";
+    div.innerHTML = `
+                    <div>
+                        <h2 class="text-xl font-bold">${hotlineText}</h2>
+                        <p class="text-lg">${hotlineNum}</p>
+                    </div>
+                    <div>
+                        <p class="text-lg">${time}</p>
+                    </div>
+    `;
+    historyContainer.appendChild(div);
   });
 }
